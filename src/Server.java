6ff0e2037c44b0383498f121477c19b2a3897ebf
Elizabeth Engelman <emengelman@gmail.com>
2014-03-11 14:56:25 -0500

    passed 404 test

--- src/Server.java
+++ src/Server.java
@@ -2,20 +2,23 @@ import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class Server{
-    public static class ServerThread extends Thread{
+public class Server {
+    public static class ServerThread extends Thread {
         Socket connectedClient = null;
         BufferedReader inputFromClient;
         PrintWriter outputToClient;
         String httpVersion;
         String requestMethod;
         String requestPath;
+        String HTML_Start = "<html><body>";
+        String HTML_END = "</body></html>";
 
-        public ServerThread(Socket newConnection){
+        public ServerThread(Socket newConnection) {
             connectedClient = newConnection;
         }
-        public void run(){
-            try{
+
+        public void run() {
+            try {
                 inputFromClient = new BufferedReader(new InputStreamReader(connectedClient.getInputStream()));
                 outputToClient = new PrintWriter(connectedClient.getOutputStream(), true);//needs the true to pass the test - autoFlush
                 String request = inputFromClient.readLine();
@@ -28,42 +31,44 @@ public class Server{
                 System.out.println("This is the request Method: " + requestMethod);
                 System.out.println("This is the request path: " + requestPath);
 
-
-                if (requestMethod.equals("GET")){
-                    outputToClient.println(httpVersion + " 200 OK\r\n");//needs the println (instead of print) in order to pass
-                    BufferedReader fileBR = new BufferedReader(new FileReader("../cob_spec/public/file1"));
-                    String currentLine;
-                    while ((currentLine = fileBR.readLine()) != null){
-                        outputToClient.println(currentLine);
+                if (requestMethod.equals("GET")) {
+                    if (requestPath.equals("/")){
+                        outputToClient.println(httpVersion + " 200 OK\r\n");//needs the println (instead of print) in order to pass
                     }
-                    outputToClient.println("<html>file1 contents</html>");
-                }
-                else if (requestMethod.equals("POST")){
+                    else if (new File("../cob_spec/public" + requestPath).exists()){
+                        outputToClient.println(httpVersion + " 200 OK\r\n");
+                        BufferedReader fileBR = new BufferedReader(new FileReader("../cob_spec/public" + requestPath));
+                        String currentLine;
+                        while ((currentLine = fileBR.readLine()) != null) {
+                            outputToClient.println(currentLine);
+                        }
+                    }
+                    else{
+                        outputToClient.println(httpVersion + " 404 Not Found\r\n");
+                        outputToClient.println("404 File Not Found");
+                        outputToClient.close();
+                    }
+                } else if (requestMethod.equals("POST")) {
                     outputToClient.println(httpVersion + " 200 OK");
-                }
-                else if (requestMethod.equals("PUT")){
+                } else if (requestMethod.equals("PUT")) {
                     outputToClient.println(httpVersion + " 200 OK");
-                }
-                else if (requestMethod.equals("HEAD")){
+                } else if (requestMethod.equals("HEAD")) {
                     outputToClient.println(httpVersion + " 200 OK");
-                }
-                else if(requestMethod.equals("OPTIONS")){
+                } else if (requestMethod.equals("OPTIONS")) {
                     outputToClient.println(httpVersion + " 200 OK");
                     outputToClient.println("Allow: GET,HEAD,POST,OPTIONS,PUT");
                 }
 
 
-            }
-            catch(IOException e){
+            } catch (IOException e) {
                 System.out.println(e);
             }
-            //***do i need this in order for the test to work? otherwise it doesn't seem to close properly
-            finally{
-                try{
+            //***do i need this in order for the test to work? otherwise it doesn't seem to close the sockets properly
+            finally {
+                try {
                     System.out.println("A client is going down, closing it's socket");
                     connectedClient.close();
-                }
-                catch(IOException e){
+                } catch (IOException e) {
                 }
             }
 
@@ -71,31 +76,33 @@ public class Server{
         }
     }
 
-    public static String getHTTPVersion(String request){
+    public static String getHTTPVersion(String request) {
         return request.split(" ")[2];
     }
 
-    public static String getMethod(String request){
+    public static String getMethod(String request) {
         return request.split(" ")[0];
     }
 
-    public static String getPath(String request){
+    public static String getPath(String request) {
         return request.split(" ")[1];
     }
-    public static void main(String[] args) throws IOException{
 
+    public static boolean isAFile(String filename){
+        return true;
+    }
+    public static void main(String[] args) throws IOException {
         int portNumber = 5000;
         System.out.println("Server started!");
         ServerSocket serverSocket = new ServerSocket(portNumber);
-        try{
-            while(true){
-            Socket newConnection = serverSocket.accept();
-            ServerThread newThread = new ServerThread(newConnection);
-            newThread.start();
-            System.out.println(newConnection.getInetAddress() + "connected");
+        try {
+            while (true) {
+                Socket newConnection = serverSocket.accept();
+                ServerThread newThread = new ServerThread(newConnection);
+                newThread.start();
+                System.out.println(newConnection.getInetAddress() + "connected");
             }
-        }
-        finally{
+        } finally {
             serverSocket.close();
         }
 
