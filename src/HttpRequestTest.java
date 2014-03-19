b398c25171f5dc0b40e27881d6e26afeac6babda
Elizabeth Engelman <emengelman@gmail.com>
2014-03-19 14:47:23 -0500

    added tests to HttpRequest for decoding parameters, and for setting the path

--- src/HttpRequestTest.java
+++ src/HttpRequestTest.java
@@ -1,4 +1,3 @@
-import org.junit.Before;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
@@ -11,38 +10,51 @@ public class HttpRequestTest {
     ByteArrayInputStream inputStream;
     HttpRequest request;
 
-    @Before
-    public void setup(){
-        inputStream = new ByteArrayInputStream(("GET / HTTP/1.1").getBytes());
+
+    private void createRequestObject(String requestString){
+        inputStream = new ByteArrayInputStream((requestString).getBytes());
         request = new HttpRequest(inputStream);
     }
 
     @Test
     public void parseTheMethod(){
+        createRequestObject("GET / HTTP/1.1");
         assertEquals("GET", request.getMethod());
     }
 
     @Test
     public void parseThePath(){
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(("GET /test_path HTTP/1.1").getBytes());
-        HttpRequest request = new HttpRequest(inputStream);
+        createRequestObject("GET /test_path HTTP/1.1");
         assertEquals("/test_path", request.getPath());
     }
 
     @Test
+    public void setThePath(){
+        createRequestObject("GET /test_path HTTP/1.1");
+        request.setPath("/");
+        assertEquals("/", request.getPath());
+    }
+
+    @Test
     public void parseTheHttpVersion(){
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(("GET /test_path HTTP/1.1").getBytes());
-        HttpRequest request = new HttpRequest(inputStream);
+        createRequestObject("GET /test_path HTTP/1.1");
         assertEquals("HTTP/1.1", request.getHttpVersion());
     }
 
     @Test
     public void breakUpParameters(){
-        ByteArrayInputStream inputStream = new ByteArrayInputStream(("GET /parameters?variable_1=test1&variable_2=test2").getBytes());
-        HttpRequest request = new HttpRequest(inputStream);
+        createRequestObject("GET /parameters?variable_1=test1&variable_2=test2");
         Map<String,String> testMap = new LinkedHashMap<String, String>();
         testMap.put("variable_1", "test1");
         testMap.put("variable_2", "test2");
         assertEquals(testMap, request.getIndividualParams());
+    }
+
+    @Test
+    public void testDecodeParameters(){
+        createRequestObject("GET /test_path HTTP/1.1");
+        String stringToDecode = "Operators%20%3C%2C%20%3E%2C%20%3D%2C%20!%3D%3B%20%2B%2C%20-%2C%20*%2C%20%26%2C%20%40%2C%20%23%2C%20%24%2C%20%5B%2C%20%5D%3A%20%22is%20that%20all%22%3F";
+        String decoded = "Operators <, >, =, !=; +, -, *, &, @, #, $, [, ]: \"is that all\"?";
+        assertEquals(decoded, request.decodeCharacters(stringToDecode));
     }
 }
