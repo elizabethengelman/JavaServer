c5dfb9ac4bb3152a21df19a7125549c9981b7b5f
Elizabeth Engelman <emengelman@gmail.com>
2014-03-19 14:36:14 -0500

    replaced string.replace logic in decodeCharacters method with URLDecoder.decode

--- src/HttpRequest.java
+++ src/HttpRequest.java
@@ -1,7 +1,5 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
+import java.net.URLDecoder;
 import java.util.LinkedHashMap;
 import java.util.Map;
 
@@ -63,23 +61,14 @@ public class HttpRequest {
         return paramPairs;
     }
 
-    public String decodeCharacters(String value){
-        String s1 = value.replace("%20", " ");
-        String s2 = s1.replace("%3C", "<");
-        String s3 = s2.replace("%2C", ",");
-        String s4 = s3.replace("%3E", ">");
-        String s5 = s4.replace("%3D", "=");
-        String s6 = s5.replace("%3B", ";");
-        String s7 = s6.replace("%2B", "+");
-        String s8 = s7.replace("%40", "@");
-        String s9 = s8.replace("%23", "#");
-        String s10 = s9.replace("%24", "$");
-        String s11 = s10.replace("%5B", "[");
-        String s12 = s11.replace("%3A", ":");
-        String s13 = s12.replace("%22", "\"");
-        String s14 = s13.replace("%3F", "?");
-        String s15 = s14.replace("%26", "&");
-        String s16 = s15.replace("%5D", "]");
-        return s16;
+    public String decodeCharacters(String value) {
+        String result = new String();
+        try{
+            result = URLDecoder.decode(value, "UTF-8");
+        }
+        catch(Exception e){
+            System.out.println(e);
+        }
+        return result;
     }
 }
